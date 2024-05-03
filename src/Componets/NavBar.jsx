194d9130734abddada311e0e86363b43236c8690
VasudevPavan07 <142978499+VasudevPavan07@users.noreply.github.com>
2024-05-03 18:13:26 +0530

    Update NavBar.jsx

--- src/Componets/NavBar.jsx
+++ src/Componets/NavBar.jsx
@@ -14,7 +14,7 @@ const NavBar = () => {
       <div className='flex items-center ml-[500px]'>
         <div className='w-28 relative  flex dropdown p-6 items-center  '>
           <button className='w-28  bg-[#e6e8e6] rounded-l-3xl justify-center pr-10 pl-10 h-10  flex gap-2 items-center' onClick={() => SetisOpen((prev) => !prev)} >Patients  {!isOpen ? (<img className='h-5' src={DownArrow} />) : (<img className='h-5' src={UpArrow} />)} </button>
-          {isOpen && <div className='bg-[#e6e8e6] absolute top-16 flex flex-col items-start p-1 w-full'> <li>1</li>
+          {isOpen && <div className='bg-[#e6e8e6] absolute top-16 flex flex-col items-start p-1 w-full z-20'> <li>1</li>
             <li>2</li>
             <li>3</li> </div>}
           <div className='flex items-center'>
